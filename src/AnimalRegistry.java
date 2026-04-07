0667e85eed7567879ccd0e884c0ab0f72c2c6105
Marlan Alfonso <alfonso.marlan1184@gmail.com>
2026-04-07 11:13:55 +0800

    Refactor Animal creation methods and prototypes

--- src/AnimalRegistry.java
+++ src/AnimalRegistry.java
@@ -4,26 +4,23 @@ public class AnimalRegistry {
     private Horse horsePrototype;
 
     public AnimalRegistry() {
+        // Initialize prototypes with default values
         sheepPrototype = new Sheep(4, "Baa", "Grass", "Dolly");
-        cowPrototype = new Cow(4, "Moo", "Grass");
-        horsePrototype = new Horse(4, "Neigh", "Hay", "Brown");
+        cowPrototype   = new Cow(4, "Moo", "Hay");
+        horsePrototype = new Horse(4, "Neigh", "Oats", "Brown");
     }
 
-    public Sheep createSheep(String name) {
+    public Animal createSheep(String name) {
         Sheep sheep = (Sheep) sheepPrototype.clone();
         sheep.setName(name);
         return sheep;
     }
 
-    public Cow createCow(String sound) {
-        Cow cow = (Cow) cowPrototype.clone();
-        cow.setSound(sound);
-        return cow;
+    public Animal createCow() {
+        return cowPrototype.clone();
     }
 
-    public Horse createHorse(String color) {
-        Horse horse = (Horse) horsePrototype.clone();
-        horse.setColor(color);
-        return horse;
+    public Animal createHorse() {
+        return horsePrototype.clone();
     }
 }
