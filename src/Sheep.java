86a5b9641a7d2a7b6749983d138657bd0a7251b4
Marlan Alfonso <alfonso.marlan1184@gmail.com>
2026-04-07 11:14:53 +0800

    Implement copy constructor for Sheep class
    Added a copy constructor for cloning Sheep instances and updated the clone method to use it.

--- src/Sheep.java
+++ src/Sheep.java
@@ -11,13 +11,17 @@ public class Sheep implements Animal {
         this.name = name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    // Copy constructor for cloning
+    private Sheep(Sheep other) {
+        this.legs = other.legs;
+        this.sound = other.sound;
+        this.food = other.food;
+        this.name = other.name;
     }
 
     @Override
     public Animal clone() {
-        return new Sheep(legs, sound, food, name);
+        return new Sheep(this);
     }
 
     @Override
@@ -29,4 +33,13 @@ public class Sheep implements Animal {
     public String getType() {
         return "Sheep";
     }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getLegs() { return legs; }
+    public void setLegs(int legs) { this.legs = legs; }
+    public String getSound() { return sound; }
+    public void setSound(String sound) { this.sound = sound; }
+    public String getFood() { return food; }
+    public void setFood(String food) { this.food = food; }
 }
